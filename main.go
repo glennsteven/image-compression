ee9fd19d337788083aef69d3d7bae687acaf6e13
glennsteven <glnsteven@gmail.com>
2024-05-29 13:47:07 +0700

    update

--- main.go
+++ main.go
@@ -22,31 +22,30 @@ func main() {
 		}
 	}()
 
-	rabbitCfg := config.RabbitMq{
-		Username:                    cfg.GetString("RABBITMQ_USERNAME"),
-		Password:                    cfg.GetString("RABBITMQ_PASSWORD"),
-		Port:                        cfg.GetString("RABBITMQ_PORT"),
-		Host:                        cfg.GetString("RABBITMQ_HOST"),
-		Topic:                       cfg.GetString("RABBITMQ_TOPIC"),
-		PathOriginalFile:            cfg.GetString("PATH_ORIGINAL_FILE"),
-		PathCompressed:              cfg.GetString("PATH_COMPRESS_FILE"),
-		SubPathOriginalInvtrypht:    cfg.GetString("SUB_PATH_ORIGINAL_INVTRYPHT"),
-		SubPathOriginalAdjdmgpht:    cfg.GetString("SUB_PATH_ORIGINAL_ADJDMGPHT"),
-		SubPathCompressionInvtrypht: cfg.GetString("SUB_PATH_COMPRESS_INVTRYPHT"),
-		SubPathCompressionAdjdmgpht: cfg.GetString("SUB_PATH_COMPRESS_ADJDMGPHT"),
+	cfgs := config.Configurations{
+		RabbitMq: config.RabbitMq{
+			Username:                    cfg.GetString("RABBITMQ_USERNAME"),
+			Password:                    cfg.GetString("RABBITMQ_PASSWORD"),
+			Port:                        cfg.GetString("RABBITMQ_PORT"),
+			Host:                        cfg.GetString("RABBITMQ_HOST"),
+			Topic:                       cfg.GetString("RABBITMQ_TOPIC"),
+			PathOriginalFile:            cfg.GetString("PATH_ORIGINAL_FILE"),
+			PathCompressed:              cfg.GetString("PATH_COMPRESS_FILE"),
+			SubPathOriginalInvtrypht:    cfg.GetString("SUB_PATH_ORIGINAL_INVTRYPHT"),
+			SubPathOriginalAdjdmgpht:    cfg.GetString("SUB_PATH_ORIGINAL_ADJDMGPHT"),
+			SubPathCompressionInvtrypht: cfg.GetString("SUB_PATH_COMPRESS_INVTRYPHT"),
+			SubPathCompressionAdjdmgpht: cfg.GetString("SUB_PATH_COMPRESS_ADJDMGPHT"),
+		},
+		Discord: config.Discord{
+			Url: cfg.GetString("URL_BOT_DISCORD"),
+		},
+		Server: config.Server{
+			Name: cfg.GetString("APP_ENV"),
+		},
+		ImageSetting: config.ImageSetting{
+			Quality: cfg.GetInt("QUALITY_COMPRESS"),
+		},
 	}
 
-	discordCfg := config.Discord{
-		Url: cfg.GetString("URL_BOT_DISCORD"),
-	}
-
-	serverCfg := config.Server{
-		Name: cfg.GetString("APP_ENV"),
-	}
-
-	imageSettingCfg := config.ImageSetting{
-		Quality: cfg.GetInt("QUALITY_COMPRESS"),
-	}
-
-	compressed.ConsumerProcessing(logger, rabbitCfg, discordCfg, serverCfg, imageSettingCfg)
+	compressed.ConsumerProcessing(logger, cfgs)
 }
