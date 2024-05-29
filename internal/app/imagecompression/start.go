0f73d623a2bf1ecb23e58590284ed8861eaf9863
glennsteven <glnsteven@gmail.com>
2024-05-29 15:19:15 +0700

    update

--- internal/app/imagecompression/start.go
+++ internal/app/imagecompression/start.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"image-compressions/compressed"
-	config2 "image-compressions/internal/config"
+	"image-compressions/internal/config"
 )
 
 func Start() error {
-	cfg, err := config2.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		return fmt.Errorf("failed to load config: %w", err)
 	}
 
-	logger, err := config2.NewLogger(cfg.Logger)
+	logger, err := config.NewLogger(cfg.Logger)
 	if err != nil {
 		return fmt.Errorf("cannot load logger: %w", err)
 	}
